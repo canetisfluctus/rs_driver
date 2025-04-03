27ee29b107a2a2a624a31b446edc9b6458726d17
uyjis <acikgoz@campus.tu-berlin.de>
2025-04-03 11:40:09 +0200

    Starting over

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver/src
+# Install script for directory: C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/pcap_capture")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/pcap_capture")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver/src/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver/src/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/acikgoez/Documents/CDS/lidar/Forked/rs_driver/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
